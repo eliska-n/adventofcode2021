905fd608aeedc5f1efffd5721e15c8a31c60203b
eliska <eliska.novotna@protonmail.com>
2021-12-06 21:12:38 +0100

    day5

--- day5/star2.py
+++ day5/star2.py
@@ -3,24 +3,27 @@ from collections import Counter
 def getpoints(coordinates):
     points = []
     for ((x1, y1), (x2, y2)) in coordinates:
+        x1 = int(x1)
+        x2 = int(x2)
+        y1 = int(y1)
+        y2 = int(y2)
         #horizontal
         if y1 == y2:
-            l = [[x, int(y1)] for x in range(min(int(x1),int(x2)), max(int(x1),int(x2))+1)]
+            l = [[x, y1] for x in range(min(x1,x2), max(x1,x2)+1)]
             points.append(l)
         #vertical
         elif x1 == x2:
-            l = [[int(x1), y] for y in range(min(int(y1),int(y2)), max(int(y1),int(y2))+1)]
+            l = [[x1, y] for y in range(min(y1,y2), max(y1,y2)+1)]
             points.append(l)
         #diagonal
         else:
-            if x1 < x2:
-                x = [x for x in range(int(x1), int(x2)+1)]
-            else:
-                x = [x for x in range(int(x1), int(x2)-1, -1)]
-            if y1 < y2:
-                y = [y for y in range(int(y1), int(y2)+1)]
-            else: 
-                y = [y for y in range(int(y1), int(y2)-1, -1)]
+            x = [x for x in range(min(x1,x2), max(x1,x2)+1)]
+            if x1 > x2:
+                x.reverse()
+            y = [y for y in range(min(y1,y2), max(y1,y2)+1)]
+            if y1 > y2:
+                y.reverse()
+
             l = [[x[i], y[i]] for i in range(len(x))]
             points.append(l)
     return points
