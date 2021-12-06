ef1ce804e29d13f885f70376ec95574e381f88b3
eliska <eliska.novotna@protonmail.com>
2021-12-06 12:52:07 +0100

    day6

---
+++ day6/star1.py
@@ -0,0 +1,25 @@
+
+with open("input.txt", encoding="utf-8") as f:
+    strings = [row.split(",") for row in f]
+    strings = strings[0]
+fishes = [int(fish) for fish in strings] # O:)
+    
+print(fishes)
+
+prevd = {i: fishes.count(i) for i in range(9)}
+print(prevd)
+
+for i in range(256):
+    nextd = {j: 0 for j in range(9)}
+    for key, value in prevd.items():
+        if key > 0:
+            nextd[key-1] = nextd[key-1] + value
+        if key == 0:
+            nextd[8] = value
+            nextd[6] = nextd[6] + value
+            
+    prevd = nextd
+
+population = sum(prevd.values())
+print(prevd)
+print(population)
