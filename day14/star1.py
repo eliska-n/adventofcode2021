8e04570274671977cfd2df0f5993b7281a773cfe
eliska <eliska.novotna@protonmail.com>
2021-12-14 21:54:16 +0100

    day14 slightly more readable

--- day14/star1.py
+++ day14/star1.py
@@ -1,8 +1,9 @@
 from collections import Counter
 
+
 def polymerize(template, instructions):
-    count = 0
-    while count < 40:
+    cycles = 0
+    while cycles < 10:
         to_insert = []
         for index, letter in enumerate(template[:len(template)-1]):
             double = letter + template[index+1]
@@ -13,24 +14,26 @@ def polymerize(template, instructions):
             new_template += template[i]
             if i < len(to_insert):
                 new_template += to_insert[i]
-
+        #restart cycle
         template = new_template
         new_template = ""
-        count += 1
-        print(count)
-    c = Counter(map(tuple, template))
-    counts = {k[0].strip(): v for k, v in c.items()}
-    
+        cycles += 1
+        
+    counts = Counter(template)
+
     return counts
+
 
 def main():
     with open("input.txt", encoding="utf-8") as f:
         inputs = [row for row in f.read().split("\n\n")]
     template = inputs[0]
-    instructions = {x.split(" -> ")[0]: x.split(" -> ")[1] for x in inputs[1].split("\n")}
+    instructions = {x.split(" -> ")[0]: x.split(" -> ")[1]
+                    for x in inputs[1].split("\n")}
     counts = polymerize(template, instructions)
     result = max(counts.values()) - min(counts.values())
     print(result)
+
 
 if __name__ == "__main__":
     main()
--- day14/star2.py
+++ day14/star2.py
@@ -1,50 +1,52 @@
 from collections import Counter
 
-def main():
-    with open("input.txt", encoding="utf-8") as f:
-        inputs = [row for row in f.read().split("\n\n")]
-    template = inputs[0]
-    instructions = {x.split(" -> ")[0]: x.split(" -> ")[1] for x in inputs[1].split("\n")}
-    template_doubles = []
-    for index, letter in enumerate(template[:len(template)-1]):
-        d = letter + template[index+1]
-        template_doubles.append(d)
-    c = Counter(map(tuple, template_doubles))
-    template_counts = {"".join(k): v for k, v in c.items()}
 
-    counter = 0
-    while counter < 40:
-        new_counts = {}
-        for double, count in template_counts.items():
-            insert = instructions[double]
-            new_d1 = double[0] + insert
-            new_d2 = insert + double[1]
-            new_ds = [new_d1, new_d2]
+def polymerize(template_counts, instructions):
+    cycles = 0
+    while cycles < 40:
+        new_counts = Counter()
+        for pair, count in template_counts.items():
+            insert = instructions[pair]
+            new_ds = [pair[0] + insert, insert + pair[1]]
             for new_d in new_ds:
-                if new_d in new_counts.keys():
-                    new_counts[new_d] += count
-                else:
-                    new_counts[new_d] = count
+                new_counts[new_d] += count
+        # restart cycle
         template_counts = new_counts
         new_counts = {}
-        counter += 1
-    
-    letter_counts = {}
-    for double, count in template_counts.items():
-        for letter in double:
-            if letter in letter_counts.keys():
-                letter_counts[letter] += count
-            else:
-                letter_counts[letter] = count
-    
+        cycles += 1
+
+    return template_counts
+
+
+def count_elements(template, polymer):
+    letter_counts = Counter()
+    for (pair), count in polymer.items():
+        for letter in pair:
+            letter_counts[letter] += count
+    # all letters are repeated twice but the first and the last ones
     first_letter = template[0]
     last_letter = template[-1]
     letter_counts[first_letter] += 1
     letter_counts[last_letter] += 1
-    letter_counts = {k: int(v/2) for k, v in letter_counts.items()}
-    
-    result = max(letter_counts.values()) - min(letter_counts.values())
+    element_counts = {k: int(v/2) for k, v in letter_counts.items()}
+    return element_counts
+
+
+def main():
+    with open("input.txt", encoding="utf-8") as f:
+        inputs = [row for row in f.read().split("\n\n")]
+    template = inputs[0]
+    instructions = {x.split(" -> ")[0]: x.split(" -> ")[1]
+                    for x in inputs[1].split("\n")}
+    template_counts = Counter(template[i]+template[i+1]
+                              for i in range(len(template)-1))
+
+    polymer = polymerize(template_counts, instructions)
+    element_counts = count_elements(template, polymer)
+
+    result = max(element_counts.values()) - min(element_counts.values())
     print(result)
+
 
 if __name__ == "__main__":
     main()
